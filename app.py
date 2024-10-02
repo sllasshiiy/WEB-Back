a7a7a44c19bb5a37bf76e78dec3cc6d164f691aa
Виктория sllasshiiiy <vika.zag004@mail.ru>
2024-10-02 21:31:24 +0700

    Фавиконка

--- app.py
+++ app.py
@@ -223,7 +223,10 @@ def start():
         </header>
 
         <main>
-        <a href="/lab1">Первая лабораторная</a>
+        <ul>
+        <li><a href="/lab1">Первая лабораторная</a></li>
+        <li><a href="/lab2/">Вторая лабораторная</a></li>
+        </ul>
         </main>
 
         <footer>
