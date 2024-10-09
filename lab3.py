a750f101a2fa883d91e51768ed05d6a3beacf3b4
Виктория sllasshiiiy <vika.zag004@mail.ru>
2024-10-09 17:43:22 +0700

    Cookie

--- lab3.py
+++ lab3.py
@@ -1,27 +1,65 @@
-from flask import Blueprint, render_template, request, make_response
+from flask import Blueprint, render_template, request, make_response, redirect
 lab3=Blueprint('lab3',__name__)
 
 
 @lab3.route('/lab3/')
 def lab():
     name=request.cookies.get('name')
-    return render_template('lab3/lab3.html', name=name)
+    name_color=request.cookies.get('name_color')
+    return render_template('lab3/lab3.html', name=name, name_color=name_color)
+
+
+@lab3.route('/lab3/del_cookie')
+def del_cookie():
+    resp=make_response(redirect('/lab3/'))
+    resp.delete_cookie('name')
+    resp.delete_cookie('age')
+    resp.delete_cookie('name_color')
+    return resp
 
 @lab3.route('/lab3/cookie')
 def cookie():
-    resp=make_response('установка cookie', 200)
-    resp.set_cookie('name', 'Alex')
+    resp=make_response(redirect('/lab3/'))
+    resp.set_cookie('name', 'Vika',max_age=5)
     resp.set_cookie('age', '20')
-    resp.set_cookie('color', 'magenta')
+    resp.set_cookie('name_color', 'magenta')
+    return resp
 
 @lab3.route('/lab3/form1')
 def form1():
     errors={}
     user=request.args.get('user')
     if user == '':
-        errors['user'] = 'Заполните поле!'
+        errors['user'] = 'Заполните поле "Имя" !'
     age=request.args.get('age')
     if age == '':
-        errors['age'] = 'Заполните поле!'
+        errors['age'] = 'Заполните поле "Имя" !'
     sex=request.args.get('sex')
     return render_template('lab3/form1.html', user=user, age=age, sex=sex, errors=errors)
+
+@lab3.route('/lab3/order')
+def order():
+    return render_template('lab3/order.html')
+
+price=0
+
+@lab3.route('/lab3/pay')
+def pay():
+    global price
+    drink=request.args.get('drink')
+    if drink == 'cofee':
+        price=120
+    elif drink == 'black-tea':
+        price=80
+    else:
+        price=70
+
+    if request.args.get('milk') == 'on':
+        price+=30
+    if request.args.get('sugar') == 'on':
+        price+=10
+    return render_template('lab3/pay.html', price=price)
+
+@lab3.route('/lab3/success')
+def success():
+    return render_template('lab3/success.html', price=price)
