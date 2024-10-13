7728668135e7eac3660bb3673e155f4058702c32
Виктория sllasshiiiy <vika.zag004@mail.ru>
2024-10-13 19:42:13 +0700

    Железнодорожный билет

--- lab3.py
+++ lab3.py
@@ -94,3 +94,66 @@ def settings():
     font_weight = request.cookies.get('font_weight')
     
     return render_template('lab3/settings.html', color=color, background_color=background_color, font_size=font_size,font_weight=font_weight)
+
+@lab3.route('/lab3/del_cookie_settings')
+def del_cookie_settings():
+    resp=make_response(redirect('/lab3/settings'))
+    resp.delete_cookie('color')
+    resp.delete_cookie('background_color')
+    resp.delete_cookie('font-size')
+    resp.delete_cookie('font-weight')
+    return resp
+
+@lab3.route('/lab3/ticket_form', methods=['GET', 'POST'])
+def ticket_form():
+    if request.method == 'POST':
+        # Получение данных из формы
+        passenger_name = request.form['name']
+        shelf_type = request.form['shelf']
+        is_bedding = 'bedding' in request.form
+        is_baggage = 'baggage' in request.form
+        age = request.form['age']
+        departure = request.form['departure']
+        destination = request.form['destination']
+        travel_date = request.form['date']
+        is_insurance = 'insurance' in request.form
+
+        # Проверка на заполнение всех полей и возраст
+        if not all([passenger_name, shelf_type, age, departure, destination, travel_date]):
+            flash("Пожалуйста, заполните все поля.")
+            return redirect(url_for('ticket_form'))
+
+        try:
+            age = int(age)
+            if age < 1 or age > 120:
+                flash("Возраст должен быть от 1 до 120 лет.")
+                return redirect(url_for('ticket_form'))
+        except ValueError:
+            flash("Возраст должен быть числом.")
+            return redirect(url_for('ticket_form'))
+
+        # Расчет стоимости
+        price = 1000 if age >= 18 else 700
+
+        if shelf_type in ['lower', 'lower_side']:
+            price += 100
+        if is_bedding:
+            price += 75
+        if is_baggage:
+            price += 250
+        if is_insurance:
+            price += 150
+
+        ticket_type = "Детский билет" if age < 18 else "Взрослый билет"
+
+        return render_template('/lab3/ticket_form.html', 
+                               name=passenger_name, 
+                               shelf=shelf_type, 
+                               age=age, 
+                               departure=departure, 
+                               destination=destination, 
+                               travel_date=travel_date, 
+                               price=price, 
+                               ticket_type=ticket_type)
+
+    return render_template('lab3/forma.html')
