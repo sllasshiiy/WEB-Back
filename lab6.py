af3ed8947da3034ce0d23cb70b51f976819bf067
Виктория sllasshiiiy <vika.zag004@mail.ru>
2024-12-04 16:20:40 +0700

    Лабораторная работа 7

--- lab6.py
+++ lab6.py
@@ -9,6 +9,91 @@ for i in range(1,11):
 @lab6.route('/lab6/')
 def main():
     return render_template('lab6/lab6.html')
+def db_connect():
+    if current_app.config['DB_TYPE']=='postgres':
+        conn=psycopg2.connect(
+            host='127.0.0.1',
+            port='5432',
+            database='vika_zag_knowledge_base',
+            user='vika_zagorodnyaya_knowledge_base',
+            password='200890',
+            options="-c client_encoding=UTF8"
+        )
+        cur=conn.cursor(cursor_factory=RealDictCursor)
+    else:
+        dir_path=path.dirname(path.realpath(__file__))
+        db_path=path.jsoin(dir_path,"database.db")
+        conn=sqlite3.connect(db_path)
+        conn.row_factory=sqlite3.Row
+        cur=conn.cursor()
+
+    return conn,cur
+
+def db_close(conn,cur):
+    conn.commit()
+    cur.close()
+    conn.close()
+
+@lab6.route('/lab6/login',methods=['GET','POST'])
+def login():
+    if request.method=='GET':
+        return render_template('lab6/login.html')
+    
+    login=request.form.get('login')
+    password=request.form.get('password')
+
+    if not (login or password):
+        return render_template('lab6/login.html',error="Заполните поля!")
+    
+    conn,cur=db_connect()
+
+    cur.execute("SELECT * FROM users WHERE login=%s;",(login, ))
+    user=cur.fetchone()
+
+    if not user:
+        db_close(conn,cur)
+        return render_template('lab6/login.html',error='Логин и/или пароль неверны')
+    
+    if not check_password_hash( user['password'],password):
+        db_close(conn,cur)
+        return render_template('lab6/login.html',error='Логин и/или пароль неверны')
+    
+    session['login']=login
+    db_close(conn,cur)
+    return render_template('lab6/succes_login.html',login=login)
+
+@lab6.route('/lab6/register',methods=['GET','POST'])
+def register():
+    if request.method=='GET':
+        return render_template('lab6/register.html')
+    
+    login=request.form.get('login')
+    password=request.form.get('password')
+
+    if not (login or password):
+        return render_template('lab6/register.html',error='Заполните все поля')
+    conn,cur=db_connect()
+
+    if current_app.config['DB_TYPE']=='postgres':
+        cur.execute("SELECT login FROM users WHERE login=%s;",(login, ))
+    else:
+        cur.execute("SELECT login FROM users WHERE login=?;",(login, ))
+    if cur.fetchone():
+        db_close(conn,cur)
+        return render_template('lab6/register.html',error="Такой пользователь уже существует")
+    
+    password_hash=generate_password_hash(password)
+    if current_app.config['DB_TYPE']=='postgres':
+        cur.execute("INSERT INTO users (login,password) VALUES (%s,%s);", (login, password_hash))
+    else:
+        cur.execute("INSERT INTO users (login,password) VALUES (?,?);", (login, password_hash))
+    db_close(conn,cur)
+    return render_template('lab6/success.html',login=login)
+
+@lab6.route('/lab6/logout')
+def logout():
+    session.pop('login',None)
+    return redirect('/lab6/login')
 
 @lab6.route('/lab6/json-rpc-api/',methods=['POST'])
 def api():
